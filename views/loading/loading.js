76cc9524501728803f98cd5f7e52a4b8d5bd0de2
Diogoxiang <a233894432@gmail.com>
2017-03-29 10:24:43 +0800

    git init

---
+++ views/loading/loading.js
@@ -0,0 +1,15 @@
+/**
+ * Created 2017/3/22
+ */
+require(["vue","common","api","dialog"],function(Vue,app,api,dialog){
+    let vm = new Vue({
+        el:"#vm",
+        data:{
+        },
+        methods:{
+        },
+        computed: {
+        }
+
+    });
+});
